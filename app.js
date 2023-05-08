2d252d13daa40559349bcfcdff45b6238d5dcb24
Gabriel Betelu <gabrielbetelu@yahoo.com.ar>
2023-05-08 20:27:45 -0300

    agregando estilos

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-let publicPath = path.resolve(__dirname , "./public");
-app.use(express.static(publicPath));
+//let publicPath = path.resolve(__dirname , "./public");
+app.use(express.static(path.resolve(__dirname , "./public")));
 
 app.listen(3000 , () => {
     console.log("Servidor corriendo en el puerto 3000");
