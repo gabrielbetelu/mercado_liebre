c20ba2c4bb5b5a82ef1cbfbc7c664877d76f4e41
Gabriel Betelu <gabrielbetelu@yahoo.com.ar>
2023-05-09 11:46:28 -0300

    correcciones de css

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-//let publicPath = path.resolve(__dirname , "./public");
-app.use(express.static(path.resolve(__dirname , "./public")));
+app.use(express.static("./public"));
 
 app.listen(3000 , () => {
     console.log("Servidor corriendo en el puerto 3000");
 });
-/*app.get("/", (req , res) => {
-    res.send("Inicio Correcto")
-});
-*/
 
 app.get("/", (req , res) => {
     res.sendFile(path.join(__dirname , "/views/home.html"));
