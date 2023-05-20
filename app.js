7697d2879c88e85af74659b3d51a7a5a8edf0f52
Gabriel Betelu <gabrielbetelu@yahoo.com.ar>
2023-05-20 17:50:14 -0300

    actualizado a responsive e inicio de formularios

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ app.listen(3000 , () => {
 app.get("/", (req , res) => {
     res.sendFile(path.join(__dirname , "/views/home.html"));
 });
+
+app.get("/register.html", (req , res) => {
+    res.sendFile(path.join(__dirname , "/views/register.html"));
+});
+
+app.get("/login.html", (req , res) => {
+    res.sendFile(path.join(__dirname , "/views/login.html"));
+});
