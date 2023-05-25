ab8415cd82668a9119c7c4b326c39a2c92b7dba5
Gabriel Betelu <gabrielbetelu@yahoo.com.ar>
2023-05-24 21:07:11 -0300

    preparación final

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const path = require('path');
 const app = express();
 app.use(express.static("./public"));
 
-app.listen(3000 , () => {
-    console.log("Servidor corriendo en el puerto 3000");
-});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
+})
 
 app.get("/", (req , res) => {
     res.sendFile(path.join(__dirname , "/views/home.html"));
